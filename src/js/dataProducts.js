4261dbe2fcd866c7370e822d9ff90fa622224111
TatianaKrivonos <konan_air.96@mail.ru>
2019-04-01 22:08:39 +0300

    вывод карточек на страницу

--- src/js/dataProducts.js
+++ src/js/dataProducts.js
@@ -1,7 +1,69 @@
 const dataProducts = {
   products:[
     {
-
-    }
+      name: 'Lorem Ipsum is simply',
+      img: 'glasses1.jpg',
+      price: '388',
+      lastPrice: '',
+      label: 'new'
+    },
+    {
+      name: 'Lorem Ipsum is simply',
+      img: 'glasses2.jpg',
+      price: '90',
+      lastPrice: '',
+      label: ''
+    },
+    {
+      name: 'Lorem Ipsum is simply',
+      img: 'glasses3.jpg',
+      price: '300',
+      lastPrice: '$460',
+      label: 'sale'
+    },
+    {
+      name: 'Lorem Ipsum is simply',
+      img: 'glasses4.jpg',
+      price: '180',
+      lastPrice: '',
+      label: ''
+    },
+    {
+      name: 'Lorem Ipsum is simply',
+      img: 'glasses5.jpg',
+      price: '278',
+      lastPrice: '',
+      label: 'new'
+    },
+    {
+      name: 'Lorem Ipsum is simply',
+      img: 'glasses6.jpg',
+      price: '378',
+      lastPrice: '',
+      label: ''
+    },
+    {
+      name: 'Lorem Ipsum is simply',
+      img: 'glasses7.jpg',
+      price: '500',
+      lastPrice: '',
+      label: 'new'
+    },
+    {
+      name: 'Lorem Ipsum is simply',
+      img: 'glasses8.jpg',
+      price: '200',
+      lastPrice: '$460',
+      label: 'sale'
+    },
+    {
+      name: 'Lorem Ipsum is simply',
+      img: 'glasses9.jpg',
+      price: '370',
+      lastPrice: '',
+      label: 'new'
+    },
   ]
 }
+
+export default dataProducts;
--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,46 @@
+import dataProducts from '../js/dataProducts.js';
+
 $(document).ready(function(){
   $('.j-slider').slick();
   $('.aside-content__slider').slick({
     dots: true,
     arrows: false
   });
-
 })
+
+const cards = document.querySelector('.products__row');
+
+function createCards(dataProducts) {
+  const dataArr = dataProducts.products;
+  let cardString = '';
+  dataArr.forEach(function(product){
+    cardString = cardString + `<div class="products__col">
+                                <article class="product-card">
+                                  <img src="img/glasses/${product.img}" alt="" class="product-card__img" width="217" height="228">
+                                  <div class="product-card__row">
+                                    <div class="product-card__info">
+                                      <h3 class="product-card__title">${product.name}</h3>
+                                      <p class="product-card__price"><span>${product.lastPrice} </span>$${product.price}</p>
+                                    </div>
+                                    <button class="product-card__btn">
+                                      <svg width="24" height="24">
+                                        <use xlink:href="img/sprite.svg#supermarket-basket"></use>
+                                      </svg>
+                                    </button>
+                                  </div>
+                                </article>
+                              </div>`;
+  });
+
+  return cardString;
+}
+
+function insertElements(dataProducts, wrap) {
+        const html = createCards(dataProducts);
+        wrap.innerHTML = html;
+}
+
+insertElements(dataProducts, cards);
+
+
+
