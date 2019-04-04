ba37583da6ac5c8a3dd4d424949db6c0f4113a1c
TatianaKrivonos <konan_air.96@mail.ru>
2019-04-04 21:23:20 +0300

    исправление размеров и отступов

--- src/js/dataProducts.js
+++ src/js/dataProducts.js
@@ -3,63 +3,63 @@ const dataProducts = {
     {
       name: 'Lorem Ipsum is simply',
       img: 'glass1.jpg',
-      price: '388',
+      price: '388.00',
       lastPrice: '',
       label: 'new'
     },
     {
       name: 'Lorem Ipsum is simply',
       img: 'glass2.jpg',
-      price: '90',
+      price: '90.00',
       lastPrice: '',
       label: ''
     },
     {
       name: 'Lorem Ipsum is simply',
       img: 'glass3.jpg',
-      price: '300',
+      price: '300.00',
       lastPrice: '$460',
       label: 'sale'
     },
     {
       name: 'Lorem Ipsum is simply',
       img: 'glass4.jpg',
-      price: '180',
+      price: '180.00',
       lastPrice: '',
       label: ''
     },
     {
       name: 'Lorem Ipsum is simply',
       img: 'glass5.jpg',
-      price: '278',
+      price: '278.00',
       lastPrice: '',
       label: 'new'
     },
     {
       name: 'Lorem Ipsum is simply',
       img: 'glass6.jpg',
-      price: '378',
+      price: '378.00',
       lastPrice: '',
       label: ''
     },
     {
       name: 'Lorem Ipsum is simply',
       img: 'glass7.jpg',
-      price: '500',
+      price: '500.00',
       lastPrice: '',
       label: 'new'
     },
     {
       name: 'Lorem Ipsum is simply',
       img: 'glass8.jpg',
-      price: '200',
+      price: '200.00',
       lastPrice: '$460',
       label: 'sale'
     },
     {
       name: 'Lorem Ipsum is simply',
-      img: 'glasses9.jpg',
-      price: '370',
+      img: 'glass9.jpg',
+      price: '370.00',
       lastPrice: '',
       label: 'new'
     },
